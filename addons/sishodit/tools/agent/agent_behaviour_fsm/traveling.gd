8c103bcf34bb3e5f913dd1b259a683e198a997b2
Xiul <xiul10901@gmail.com>
2023-02-06 15:43:28 +0100

    Deleted the path drawing for agent given that now godot includes it for debug

--- addons/sishodit/tools/agent/agent_behaviour_fsm/traveling.gd
+++ addons/sishodit/tools/agent/agent_behaviour_fsm/traveling.gd
@@ -6,8 +6,7 @@ func on_process(_delta: float):
 	if my_agent.nav_agent.is_navigation_finished():
 		transitioned_to.emit("DoingStep")
 		return
-	
-	#_draw_path(my_agent.nav_agent.get_current_navigation_path())
+
 	var next_pos = my_agent.nav_agent.get_next_path_position()
 	var vel = my_agent.speed*(next_pos-my_agent.global_position).normalized()
 	
@@ -28,20 +27,3 @@ func on_enter():
 
 func on_exit():
 	pass
-
-## Aux functions ##
-func _draw_path(path: PackedVector3Array, color = Color.RED) -> MeshInstance3D:
-	var mesh = mesh_instance.mesh as ImmediateMesh
-	var material := ORMMaterial3D.new()
-	mesh.clear_surfaces()
-
-	for i in path.size()-1:
-		mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
-		mesh.surface_add_vertex(path[i])
-		mesh.surface_add_vertex(path[i+1])
-		mesh.surface_end()	
-	
-	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	material.albedo_color = color
-	
-	return mesh_instance
