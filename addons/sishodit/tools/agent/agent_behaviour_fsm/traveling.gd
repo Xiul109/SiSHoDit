4f5d97d60853d6767ab38d053b4081db06836006
Xiul <xiul10901@gmail.com>
2023-02-24 16:33:47 +0100

    Now, the agent target is shown visually

--- addons/sishodit/tools/agent/agent_behaviour_fsm/traveling.gd
+++ addons/sishodit/tools/agent/agent_behaviour_fsm/traveling.gd
@@ -1,6 +1,25 @@
 extends State
 
 var mesh_instance : MeshInstance3D
+var mesh_texture = preload("res://addons/sishodit/assets/target_position.png")
+
+func _ready():
+	# Setting the mesh instance
+	mesh_instance = MeshInstance3D.new()
+	mesh_instance.mesh = QuadMesh.new()#ImmediateMesh.new()
+	mesh_instance.mesh.size = Vector2(3.5,3.5)
+	mesh_instance.top_level = true
+	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	# Setting the material
+	var mesh_material = StandardMaterial3D.new()
+	mesh_material.billboard_mode = BaseMaterial3D.BILLBOARD_ENABLED
+	mesh_material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	mesh_material.no_depth_test = true
+	mesh_material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+	mesh_material.albedo_texture = mesh_texture
+	
+	mesh_instance.mesh.surface_set_material(0, mesh_material)
+	add_child(mesh_instance)
 
 func on_process(_delta: float):
 	if my_agent.nav_agent.is_navigation_finished():
@@ -17,13 +36,11 @@ func on_process(_delta: float):
 	my_agent.move_and_slide()
 	if abs(Vector3.UP.cross(vel).length()) > 0.001:
 		my_agent.look_at(next_pos)
+	
+
 
 func on_enter():
-	mesh_instance = MeshInstance3D.new()
-	mesh_instance.mesh = ImmediateMesh.new()
-	mesh_instance.cast_shadow = false
-	
-	get_tree().get_root().add_child.call_deferred(mesh_instance)
+	mesh_instance.position = my_agent.nav_agent.target_position
 
 func on_exit():
 	pass
