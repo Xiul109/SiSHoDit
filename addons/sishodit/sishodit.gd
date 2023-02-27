e2e3f716400b8ddff3de3df5eac07f38bf6fcf54
Xiul <xiul10901@gmail.com>
2023-02-27 11:57:08 +0100

    Removed the autoload from the plugin to avoid errors

--- addons/sishodit/sishodit.gd
+++ addons/sishodit/sishodit.gd
@@ -1,14 +1,10 @@
 @tool
 extends EditorPlugin
 
-const TIME_SIM_NAME = "TimeSim"
-const EVENT_LOGGER_NAME = "EventLogger"
 
 func _enter_tree():
-	add_autoload_singleton(TIME_SIM_NAME, "res://addons/sishodit/tools/autoloads/time_sim.gd")
-	add_autoload_singleton(EVENT_LOGGER_NAME, "res://addons/sishodit/tools/autoloads/event_logger.gd")
+	pass
 
 
 func _exit_tree():
-	remove_autoload_singleton(TIME_SIM_NAME)
-	remove_autoload_singleton(EVENT_LOGGER_NAME)
+	pass
