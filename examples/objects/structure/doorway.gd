c8f88dc070828e9806b157fd413c19b631ab1afa
Xiul <xiul10901@gmail.com>
2023-05-18 10:54:01 +0200

    Fixed door sensors not triggering

--- examples/objects/structure/doorway.gd
+++ examples/objects/structure/doorway.gd
@@ -4,6 +4,7 @@ extends StaticBody3D
 @export var autocloses = false
 
 @onready var door = $"doorway(Clone)/door"
+@onready var sensor = $Sensor
 
 var state = "close"
 
@@ -14,10 +15,10 @@ func open():
 	if state != "open":
 		state = "open"
 		door.rotate_y(90)
-		get_node("Sensor").activate(1)
+		sensor.activate(1.0)
 
 func close():
 	if state != "close":
 		state = "close"
 		door.rotate_y(-90)
-		get_node("Sensor").activate(0)
+		sensor.activate(0.0)
