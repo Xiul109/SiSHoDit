e6141f864201a78d5501f02d6de10bb8190f3723
Xiul <xiul10901@gmail.com>
2024-02-06 13:50:45 +0100

    Changed the object reposition to a child instead of parent
    It makes much more sense this way

--- addons/sishodit/tools/objects/object_reposition.gd
+++ addons/sishodit/tools/objects/object_reposition.gd
@@ -1,42 +1,30 @@
 class_name ObjectReposition
 extends Node3D
 
-## This Node steal the groups and Usable nodes of child object. Its main function is to move the
-## position of an object to improve pathfinding. It is not advisable to use it for objects that have
-## subchildrens with redirect usables.
+## This Node steal the groups and copy Usable nodes of parent object using
+## redirect_usables. Its main function is to move the position of an object to
+## improve pathfinding.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var child = _check_and_get_child()
-	_steal_groups(child)
-	_steal_usable(child)
-
-## Copies and removes the groups from the child argument
-func _steal_groups(child:Node):
-	var child_groups = child.get_groups()
-	for group in child_groups:
-		add_to_group(group)
-		child.remove_from_group(group)
-
-## Moves the Usable object from the child argument if it has any
-func _steal_usable(child:Node):
-	for node in child.get_children():
-		if node is Usable:
-			child.remove_child(node)
-			add_child(node)
-			break
-
-## Checks if the child tree has the correct format and returns the child if correct.
-func _check_and_get_child() -> Node:
-	var children = get_children()
-	
-	if children.is_empty():
+	var parent = get_parent()
+	if not parent:
 		push_warning("ObjectReposition does not have a child.")
 		return
-	
-	if children.size() > 1:
-		push_warning("ObjectReposition should only have one child. Only the first one will be used.")
-	
-	var child = children[0]
-	
-	return child
+	_steal_groups(parent)
+	_make_redirect_usables(parent)
+
+## Copies and removes the groups from the child argument
+func _steal_groups(node:Node):
+	var node_groups = node.get_groups()
+	for group in node_groups:
+		add_to_group(group)
+		node.remove_from_group(group)
+
+## Moves the Usable object from the child argument if it has any
+func _make_redirect_usables(node:Node):
+	for child in node.get_children():
+		if child is Usable:
+			var redirect_usable = RedirectUsable.new()
+			redirect_usable.usable = child
+			add_child(redirect_usable)
