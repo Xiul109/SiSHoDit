c351758c159b2ba58da07ba75cf10160cbddb7f9
Luis Cabañero Gómez <luis.cabanero@uclm.es>
2025-05-26 13:59:09 +0200

    Fixed a typo

--- addons/sishodit/presets/context_managers/calendar.gd
+++ addons/sishodit/presets/context_managers/calendar.gd
@@ -7,18 +7,18 @@ extends Simulable
 @export_range(0, 59) var minute : int
 @export_range(0, 60-0.001) var second : float
 
-var _timestampt : float = 0
+var _timestamp : float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	simulated.connect(_on_simulated)
-	_timestampt += Time.get_unix_time_from_datetime_dict({"year":year, "month":month, "day":day,
+	_timestamp += Time.get_unix_time_from_datetime_dict({"year":year, "month":month, "day":day,
 													"hour":hour, "minute":minute, "second":second})
 	# Add an initial log with the original context data as a datetime
 
 
 func _on_simulated(delta):
-	_timestampt += delta
-	var time_dict = Time.get_datetime_dict_from_unix_time(_timestampt)
-	time_dict["second"] += fmod(_timestampt, 1.0)
+	_timestamp += delta
+	var time_dict = Time.get_datetime_dict_from_unix_time(_timestamp)
+	time_dict["second"] += fmod(_timestamp, 1.0)
 	context.merge(time_dict, true)
